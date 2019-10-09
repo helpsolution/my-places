28032fc6fde05944297d980a05634a3a0bce73e1
mandarinka <investormephi@gmail.com>
2019-10-09 16:08:51 +0800

    Added table view and custom cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by freelance on 09/10/2019.
+//  Copyright © 2019 freelance. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgeOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+     
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
